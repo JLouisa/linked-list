048109fe8e96b98fe172d2dddde30cee4f2c5533
Jlouisa <jonathan.louisa@gmail.com>
2023-08-29 16:41:30 +0200

    Add manual linked list creation

--- javascript.js
+++ javascript.js
@@ -1,107 +1,139 @@
 //! Creating Nodes
-//* Node Factory
-const nodeFactory = (name) => {
-  return { name, nextNode: null };
-};
-const adamNode = nodeFactory("Adam");
-console.log(adamNode);
+// //* Node Factory
+// const nodeFactory = (name) => {
+//   return { name, nextNode: null };
+// };
+// const adamNode = nodeFactory("Adam");
+// console.log(adamNode);
 
-const eveNode = nodeFactory("Eve");
-console.log(eveNode);
+// const eveNode = nodeFactory("Eve");
+// console.log(eveNode);
 
 //* Node Class
 class NodeClass {
-  constructor(name) {
-    (this.name = name), (this.nextNode = null);
+  constructor(value) {
+    this.value = value;
+    this.nextNode = null;
   }
 }
-const adamNodeClass = new NodeClass("adam");
-console.log(adamNodeClass);
 
-const eveNodeClass = new NodeClass("eve");
-console.log(eveNodeClass);
+//? +++++++++++++++++++++++++++++++
+//Manual creation
+const a = new NodeClass("Adam");
+const c = new NodeClass("Cain");
+const e = new NodeClass("Eve");
+const n = new NodeClass("Noah");
+
+// Manual Pointing
+a.nextNode = c;
+c.nextNode = e;
+e.nextNode = n;
+//Manually print
+// console.log(a);
+// console.log(c);
+// console.log(e);
+// console.log(n);
+
+//Automatically print
+function printLinkedList(head) {
+  let current = head;
+  while (current !== null) {
+    console.log(current.value);
+    current = current.nextNode;
+  }
+}
+
+printLinkedList(a);
+
+//? +++++++++++++++++++++++++++++++
 
 //! LinkedList
-//* Factory LinkList
-const linkedListNode = (node) => {
-  return {};
-};
-
 //* Class LinkedList
 class Linkedlist {
-  constructor(name, nextNode) {
-    super(name, nextNode);
+  constructor() {
+    this.name = null;
+    this.nextNode = null;
   }
-  append(node) {
-    if (node.nextNode === null) {
-      return (node.nextNode = node.name);
-    }
-    return this.append(node.nextNode);
-  }
-  prepand(node) {
-    if (node.nextNode !== null) {
-      node.name = node.nextNode;
-    }
-    this.prepand(node.name);
-  }
-  size(node) {
-    let count = 0;
-    if (node.nextNode !== null) {
-      count++;
-    }
-    if (node.nextNode === null) {
-      return count;
-    }
-    this.size(node.nextNode);
-  }
-  head(node) {
-    if (node.name === "Head") {
-      return node.name;
-    }
-  }
-  tail(node) {
-    if (this.node.nextNode === null) {
-      return this.node.name;
-    }
-    this.tail(node.nextNode);
-  }
-  at(index) {
-    let i = 0;
-    if (i === index) {
-      return node;
-    } else {
-      node.nextNode;
-      i++;
-    }
-  }
-  pop(node) {
-    if (node.nextNode.node.nextNode === null) {
-      node.nextNode = null;
-    }
-    return pop(node.nextNode);
-  }
-  contains(name) {
-    if (node.name === name) {
-      return true;
-    }
-    if (node.next === null) {
-      return false;
-    }
-    return this.contains(node.next.name);
-  }
-  find(name) {
-    let ii = 0;
-    if (node.next === name) {
-      return ii;
-    }
-    ii++;
-    return this.find(node.next.name);
-  }
-  toString() {
-    if (node.next !== null) {
-      JSON.stringify(node);
-    } else {
-      return;
-    }
-  }
+  //   append(data) {
+  //     if (this.nextNode === null) {
+  //       const node = new NodeClass(data, this.nextNode);
+  //       this.data = this.name;
+  //       this.nextNode = node;
+  //       return node;
+  //     } else if (this.nextNode !== null) {
+  //       return this.append(this.nextNode.data);
+  //     }
+  //   }
+  //   prepand(node) {
+  //     if (node.nextNode !== null) {
+  //       node.name = node.nextNode;
+  //     }
+  //     this.prepand(node.name);
+  //   }
+  //   size(node) {
+  //     let count = 0;
+  //     if (node.nextNode !== null) {
+  //       count++;
+  //     }
+  //     if (node.nextNode === null) {
+  //       return count;
+  //     }
+  //     this.size(node.nextNode);
+  //   }
+  //   head(node) {
+  //     if (node.name === "Head") {
+  //       return node.name;
+  //     }
+  //   }
+  //   tail(node) {
+  //     if (this.node.nextNode === null) {
+  //       return this.node.name;
+  //     }
+  //     this.tail(node.nextNode);
+  //   }
+  //   at(index) {
+  //     let i = 0;
+  //     if (i === index) {
+  //       return node;
+  //     } else {
+  //       node.nextNode;
+  //       i++;
+  //     }
+  //   }
+  //   pop(node) {
+  //     if (node.nextNode.node.nextNode === null) {
+  //       node.nextNode = null;
+  //     }
+  //     return pop(node.nextNode);
+  //   }
+  //   contains(name) {
+  //     if (node.name === name) {
+  //       return true;
+  //     }
+  //     if (node.next === null) {
+  //       return false;
+  //     }
+  //     return this.contains(node.next.name);
+  //   }
+  //   find(name) {
+  //     let ii = 0;
+  //     if (node.next === name) {
+  //       return ii;
+  //     }
+  //     ii++;
+  //     return this.find(node.next.name);
+  //   }
+  //   toString() {
+  //     if (node.next !== null) {
+  //       JSON.stringify(node);
+  //     } else {
+  //       return;
+  //     }
+  //   }
 }
+
+// Todo =======================================
+// const myLinkedList = new Linkedlist();
+// myLinkedList.append("Adam");
+// myLinkedList.append("Eve");
+// Todo =======================================
